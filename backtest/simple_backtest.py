744b31f53d628dcd600d410be4331cddac1c79bf
wsy <631535207@qq.com>
2023-04-06 15:02:18 +0800

    #7 重构简单回测到类，方便其他策略

--- backtest/simple_backtest.py
+++ backtest/simple_backtest.py
@@ -10,28 +10,38 @@ from data_process.data_processor import DataProcessor
 from data_process.generate_Vt import generate_xt
 
 
-def simple_backtest():
-    roll = 20
-    etf_map = {
-        1: "SPVXSP",
-        2: "SPVIX2ME",
-        3: "SPVIX3ME",
-        4: "SPVIX4ME",
-        5: "SPVXMP",
-        6: "SPVIX6ME",
-    }
-    xt = generate_xt().fillna(method="bfill")
-    trading_price = DataProcessor().generate_trade_price().fillna(method="bfill")
-    xt = pd.merge(xt, trading_price, left_index=True, right_index=True)
-    xt["vix_ma"] = xt["ln_VIX"].rolling(window=roll).mean()
-    xt["vix_std"] = xt["ln_VIX"].rolling(window=roll).std()
+class SimpleBacktest:
+    """基于Xt和ETF的矩阵回测"""
 
-    std_times = 2
-    position_num = 1
-    net_values = []
-    init_equity = 100000
-    for month in range(1, 7):
+    def __init__(self):
 
+        self.data = self._feature_and_etfprice()
+
+    @staticmethod
+    def _feature_and_etfprice() -> pd.DataFrame:
+        etf_map = {
+            "SPVXSP": "ETF1",
+            "SPVIX2ME": "ETF2",
+            "SPVIX3ME": "ETF3",
+            "SPVIX4ME": "ETF4",
+            "SPVXMP": "ETF5",
+            "SPVIX6ME": "ETF6",
+        }
+        xt = generate_xt().fillna(method="bfill")
+        trading_price = DataProcessor().generate_trade_price().fillna(method="bfill")
+        xt = pd.merge(xt, trading_price, left_index=True, right_index=True).rename(
+            columns=etf_map
+        )
+        return xt
+
+    def _generate_signal(self, month) -> pd.DataFrame:
+
+        xt = self.data.copy()
+        roll = 20
+        xt["vix_ma"] = xt["ln_VIX"].rolling(window=roll).mean()
+        xt["vix_std"] = xt["ln_VIX"].rolling(window=roll).std()
+        std_times = 2
+        position_num = 1
         buy_signal = xt["ln_V" + str(month)] <= (
             xt["vix_ma"] - std_times * xt["vix_std"]
         )
@@ -39,60 +49,67 @@ def simple_backtest():
             xt["vix_ma"] + std_times * xt["vix_std"]
         )
 
-        xt["signal" + str(month)] = (
-            position_num * buy_signal - position_num * sell_signal
-        )
+        xt["signal"] = position_num * buy_signal - position_num * sell_signal
+        return xt
 
-        # 简单规则：
-        xt["signal" + str(month)] = (
-            xt["signal" + str(month)]
-            .replace(0, np.nan)
-            .fillna(method="ffill")
-            .replace(np.nan, 0)
-        )
-        xt["trading_flag" + str(month)] = (
-            xt["signal" + str(month)] - xt["signal" + str(month)].shift(1)
-        ).fillna(0)
-        xt["trading_price" + str(month)] = xt[etf_map[month]]
+    def run(self):
 
-        cash = []
-        position_list = []
-        equity_list = []
-        for i in range(len(xt)):
-            if i == 0:
-                equity = init_equity
-            else:
-                equity = cash[-1] + xt["trading_price" + str(month)].iloc[i] * position_list[-1]
-            equity_list.append(equity)
+        net_values = []
+        init_equity = 100000
+        for month in range(1, 7):
 
-            if xt["trading_flag" + str(month)].iloc[i] > 0:
-                position = equity / xt["trading_price" + str(month)].iloc[i]
-            elif xt["trading_flag" + str(month)].iloc[i] < 0:
-                position = -equity / xt["trading_price" + str(month)].iloc[i]
-            else:
+            xt = self._generate_signal(month)
+
+            # 简单规则：
+            xt["trading_flag"] = (
+                xt["signal"]
+                .replace(0, np.nan)
+                .fillna(method="ffill")
+                .replace(np.nan, 0)
+            )
+            xt["trading_flag"] = (
+                xt["trading_flag"] - xt["trading_flag"].shift(1)
+            ).fillna(0)
+            xt["trading_price"] = xt["ETF" + str(month)]
+
+            cash = []
+            position_list = []
+            equity_list = []
+            for i in range(len(xt)):
                 if i == 0:
-                    position = 0
+                    equity = init_equity
                 else:
-                    position = position_list[-1]
-            position_list.append(position)
+                    equity = cash[-1] + xt["trading_price"].iloc[i] * position_list[-1]
+                equity_list.append(equity)
 
-            this_cash = equity - position * xt["trading_price" + str(month)].iloc[i]
-            cash.append(this_cash)
-        xt["cash" + str(month)] = cash
-        xt["total_equity" + str(month)] = equity_list
-        xt["position" + str(month)] = position_list
+                if xt["trading_flag"].iloc[i] > 0:
+                    position = equity / xt["trading_price"].iloc[i]
+                elif xt["trading_flag"].iloc[i] < 0:
+                    position = -equity / xt["trading_price"].iloc[i]
+                else:
+                    if i == 0:
+                        position = 0
+                    else:
+                        position = position_list[-1]
+                position_list.append(position)
 
-        xt["equity" + str(month)] = (
-                xt["position" + str(month)] * xt["trading_price" + str(month)]
-        )
+                this_cash = equity - position * xt["trading_price"].iloc[i]
+                cash.append(this_cash)
+            xt["cash" + str(month)] = cash
+            xt["total_equity" + str(month)] = equity_list
+            xt["position" + str(month)] = position_list
 
-        net_value = (xt["total_equity" + str(month)] / init_equity).rename(
-            "net_value" + str(month)
-        )
-        net_values.append(net_value)
-    result = pd.concat(net_values, axis=1)
-    result.to_csv("vix_reversion_strategy.csv")
-    return result
+            xt["equity" + str(month)] = (
+                xt["position" + str(month)] * xt["trading_price"]
+            )
+
+            net_value = (xt["total_equity" + str(month)] / init_equity).rename(
+                "net_value" + str(month)
+            )
+            net_values.append(net_value)
+        result = pd.concat(net_values, axis=1)
+        result.to_csv("vix_reversion_strategy.csv")
+        return result
 
 
-simple_backtest()
+SimpleBacktest().run()
