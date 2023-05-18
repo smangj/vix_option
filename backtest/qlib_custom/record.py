c3f1e2cba7cd274e36d50bb9f9d034fce522cec6
smangj <631535207@qq.com>
2023-05-18 21:56:27 +0800

    #7 simple_backtest搬迁中

--- backtest/qlib_custom/record.py
+++ backtest/qlib_custom/record.py
@@ -261,7 +261,7 @@ class StandalonePortAnaRecord(PortAnaRecord):
         )
 
 
-class SimpleBacktestRecord(PortAnaRecord, abc.ABC):
+class SimpleBacktestRecord(PortAnaRecord):
     """
     调用SimpleBacktest回测并保存相关结果
     只支持根据score生成signal的简单策略
@@ -291,7 +291,90 @@ class SimpleBacktestRecord(PortAnaRecord, abc.ABC):
 
     def _generate(self, *args, **kwargs):
         pred = self.load("pred.pkl")
-        assert pred
+
+        assert "instrument" in pred.index.names
+        instruments = set(pred.index.levels[pred.index.names.index("instrument")])
+
+        net_values = []
+        for instrument in instruments:
+            xt = self._generate_signal(pred.loc[(slice(None), instrument), :])
+            net_values.append(xt)
+
+        # replace the "<PRED>" with prediction saved before
+        placeholder_value = {"<PRED>": pred}
+        # for k in "executor_config", "strategy_config":
+        #     setattr(self, k, fill_placeholder(getattr(self, k), placeholder_value))
+
+        # if the backtesting time range is not set, it will automatically extract time range from the prediction file
+        dt_values = pred.index.get_level_values("datetime")
+        if self.backtest_config["start_time"] is None:
+            self.backtest_config["start_time"] = dt_values.min()
+        # if self.backtest_config["end_time"] is None:
+        #     self.backtest_config["end_time"] = get_date_by_shift(dt_values.max(), 1)
+
+        # custom strategy and get backtest
+        portfolio_metric_dict, indicator_dict = normal_backtest(
+            executor=self.executor_config, strategy=self.strategy_config, **self.backtest_config
+        )
+        for _freq, (report_normal, positions_normal) in portfolio_metric_dict.items():
+            self.save(**{f"report_normal_{_freq}.pkl": report_normal})
+            self.save(**{f"positions_normal_{_freq}.pkl": positions_normal})
+
+        for _freq, indicators_normal in indicator_dict.items():
+            self.save(**{f"indicators_normal_{_freq}.pkl": indicators_normal[0]})
+            self.save(**{f"indicators_normal_{_freq}_obj.pkl": indicators_normal[1]})
+
+        for _analysis_freq in self.risk_analysis_freq:
+            if _analysis_freq not in portfolio_metric_dict:
+                warnings.warn(
+                    f"the freq {_analysis_freq} report is not found, please set the corresponding env with `generate_portfolio_metrics=True`"
+                )
+            else:
+                report_normal, _ = portfolio_metric_dict.get(_analysis_freq)
+                analysis = dict()
+                analysis["excess_return_without_cost"] = risk_analysis(
+                    report_normal["return"] - report_normal["bench"], freq=_analysis_freq
+                )
+                analysis["excess_return_with_cost"] = risk_analysis(
+                    report_normal["return"] - report_normal["bench"] - report_normal["cost"], freq=_analysis_freq
+                )
+
+                analysis_df = pd.concat(analysis)  # type: pd.DataFrame
+                # log metrics
+                analysis_dict = flatten_dict(analysis_df["risk"].unstack().T.to_dict())
+                self.recorder.log_metrics(**{f"{_analysis_freq}.{k}": v for k, v in analysis_dict.items()})
+                # save results
+                self.save(**{f"port_analysis_{_analysis_freq}.pkl": analysis_df})
+                logger.info(
+                    f"Portfolio analysis record 'port_analysis_{_analysis_freq}.pkl' has been saved as the artifact of the Experiment {self.recorder.experiment_id}"
+                )
+                # print out results
+                pprint(f"The following are analysis results of benchmark return({_analysis_freq}).")
+                pprint(risk_analysis(report_normal["bench"], freq=_analysis_freq))
+                pprint(f"The following are analysis results of the excess return without cost({_analysis_freq}).")
+                pprint(analysis["excess_return_without_cost"])
+                pprint(f"The following are analysis results of the excess return with cost({_analysis_freq}).")
+                pprint(analysis["excess_return_with_cost"])
+
+        for _analysis_freq in self.indicator_analysis_freq:
+            if _analysis_freq not in indicator_dict:
+                warnings.warn(f"the freq {_analysis_freq} indicator is not found")
+            else:
+                indicators_normal = indicator_dict.get(_analysis_freq)[0]
+                if self.indicator_analysis_method is None:
+                    analysis_df = indicator_analysis(indicators_normal)
+                else:
+                    analysis_df = indicator_analysis(indicators_normal, method=self.indicator_analysis_method)
+                # log metrics
+                analysis_dict = analysis_df["value"].to_dict()
+                self.recorder.log_metrics(**{f"{_analysis_freq}.{k}": v for k, v in analysis_dict.items()})
+                # save results
+                self.save(**{f"indicator_analysis_{_analysis_freq}.pkl": analysis_df})
+                logger.info(
+                    f"Indicator analysis record 'indicator_analysis_{_analysis_freq}.pkl' has been saved as the artifact of the Experiment {self.recorder.experiment_id}"
+                )
+                pprint(f"The following are analysis results of indicators({_analysis_freq}).")
+                pprint(analysis_df)
 
     def _generate_signal(self, month) -> pd.DataFrame:
         raise NotImplementedError
