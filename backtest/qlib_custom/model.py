cbc3d7dd4273cf204e8c8e3522f6c156cb336d37
wsy <631535207@qq.com>
2023-06-06 10:20:11 +0800

    run more model

--- backtest/qlib_custom/model.py
+++ backtest/qlib_custom/model.py
@@ -3,8 +3,13 @@
 # @Time     : 2023/4/11 16:51
 # @Author   : wsy
 # @email    : 631535207@qq.com
-from qlib.contrib.model import LinearModel
+from qlib.contrib.model import LinearModel, XGBModel
 import statsmodels.api as sm
+import pandas as pd
+import xgboost as xgb
+from qlib.data.dataset.handler import DataHandlerLP
+from qlib.data.dataset import DatasetH
+from typing import Text, Union
 
 
 class SmLinearModel(LinearModel):
@@ -25,3 +30,15 @@ class SmLinearModel(LinearModel):
         self.coef_ = result.params[1:]
         self.intercept_ = result.params[0]
         self.summary = str(result.summary())
+
+
+class XgbFix(XGBModel):
+    def predict(self, dataset: DatasetH, segment: Union[Text, slice] = "test"):
+        if self.model is None:
+            raise ValueError("model is not fitted yet!")
+        x_test = dataset.prepare(
+            segment, col_set="feature", data_key=DataHandlerLP.DK_I
+        )
+        return pd.Series(
+            self.model.predict(xgb.DMatrix(x_test.values)), index=x_test.index
+        )
