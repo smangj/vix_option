c89d18c2baa95496f686a1a0939cd4e94012fbde
wsy <631535207@qq.com>
2023-12-08 16:23:01 +0800

    加入sever_type参数 方便不同地点配置uri

--- run_qlib.py
+++ run_qlib.py
@@ -4,15 +4,20 @@
 # @Author   : wsy
 # @email    : 631535207@qq.com
 import os
+
 import fire
 from qlib.workflow.cli import workflow
 
-from backtest.qlib_custom.pipeline import myworkflow
+from backtest.qlib_custom.pipeline import myworkflow, MlflowType
 
 LEGAL_WORKFLOW = ["origin", "dynamic"]
 
 
-def main(name: str = "JiaQiBenchRecord.yaml", work_flow: str = "dynamic"):
+def main(
+    name: str = "JiaQiBenchRecord.yaml",
+    work_flow: str = "dynamic",
+    server_type: str = "local",
+):
     assert work_flow in LEGAL_WORKFLOW
     if work_flow == "origin":
         func = workflow
@@ -22,12 +27,24 @@ def main(name: str = "JiaQiBenchRecord.yaml", work_flow: str = "dynamic"):
     else:
         raise NotImplementedError
 
+    server_type = MlflowType.from_name(server_type)
+
     file_path = os.path.join("yaml_config", name)
     if os.path.isfile(file_path):
-        func(file_path, experiment_name=name.split(".")[0])
+        if work_flow == "dynamic":
+            func(file_path, experiment_name=name.split(".")[0], server_type=server_type)
+        elif work_flow == "origin":
+            func(file_path, experiment_name=name.split(".")[0])
     elif os.path.isdir(file_path):
         for file in os.listdir(file_path):
-            func(os.path.join(file_path, file), experiment_name=file.split(".")[0])
+            if work_flow == "dynamic":
+                func(
+                    os.path.join(file_path, file),
+                    experiment_name=file.split(".")[0],
+                    server_type=server_type,
+                )
+            elif work_flow == "origin":
+                func(os.path.join(file_path, file), experiment_name=name.split(".")[0])
     else:
         raise NotImplementedError
 
